640c49f4c8feecf59200d0285564c9a9814cf3e9
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2020-11-17 15:32:47 +0530

    queue w list impl

---
+++ Python/allImpl.py
@@ -0,0 +1,27 @@
+
+stack = []
+
+stack.append(1)
+stack.append(2)
+stack.append(2)
+
+print(stack)
+
+print(stack.pop())
+print(stack.pop())
+
+print(stack)
+
+
+queue = []
+
+queue.append(1)
+queue.append(2)
+queue.append(3)
+
+print(queue)
+
+print(queue.pop(0))
+print(queue.pop(0))
+
+print(queue)
